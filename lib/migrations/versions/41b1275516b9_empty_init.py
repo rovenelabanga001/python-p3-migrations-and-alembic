9b3776008f56238f2278e50a16fdd11ba6cc67e6
rovenelabanga <rovenelabanga2001@gmail.com>
2024-12-14 14:28:34 +0300

    uses alembic to autogenerate migrations

---
+++ lib/migrations/versions/41b1275516b9_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 41b1275516b9
+Revises: 
+Create Date: 2024-12-14 14:03:45.468370
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '41b1275516b9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
